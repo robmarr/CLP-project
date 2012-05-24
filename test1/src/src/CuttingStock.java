4a8fdf6a0dababf7b4449c17f8580e5551db2974
Michał Konopacki <konopacki.m@gmail.com>
2012-05-24 22:54:54 +0200

    Signed-off-by: Michał Konopacki <konopacki.m@gmail.com>
    Added resource from previous laboratory

---
+++ test1/src/src/CuttingStock.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import JaCoP.core.IntVar;
+import JaCoP.core.Store;
+import JaCoP.core.Var;
+/**
+ *
+ * @author eave
+ */
+public class CuttingStock {
+    
+}
---
+++ test1/src/src/Test1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+import gui.Window;
+import JaCoP.core.*;
+import JaCoP.constraints.*;
+import JaCoP.search.*;
+import JaCoP.set.core.*;
+import JaCoP.set.constraints.*;
+import JaCoP.set.search.*;
+
+
+/**
+ *
+ * @author eave
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world! Jestes gupi!\n");
+        Window okno = new Window();
+        okno.setVisible(true);
+        
+    }
+}
